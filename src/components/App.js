31c11703bc7d2ffc99b1337868373ea8b4459d3d
Hidehiro Nagaoka <kuinige@gmail.com>
2019-08-30 20:37:00 +0900

    Added style to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row">
+            <header style={{ margin: '20px 0 40px 0' }} className="App-header col col-12">
+              <h1>React Todo App</h1>
+            </header>
+            <main className="col col-12">
+              <form style={{ marginBottom: '20px' }}>
+                <input className="form-control"  type="text" placeholder="Enter todo here... [Press Enter]" autoComplete="off" />
+              </form>
+              <ul className="todos list-groups" style={{ padding: 0 }}>
+                <li className="todo list-group-item">
+                  <input className="form-control" type="checkbox" />
+                  <span style={{
+                    top: 0,
+                    bottom: 0,
+                    left: '3rem',
+                    right: '5rem',
+                    lineHeight: '62px',
+                    display: 'block',
+                    position: 'absolute'
+                  }}
+                  >
+                    Write this code
+                  </span>
+                  <button type="button"
+                    className="btn btn-sm btn-danger"
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      bottom: 0,
+                      right: '1.25rem',
+                      margin: 'auto 0',
+                      height: '25px',
+                      paddingTop: 0,
+                      paddingBottom: 0,
+                    }}>&times;</button>
+                </li>
+              </ul>
+            </main>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
