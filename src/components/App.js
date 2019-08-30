252ef5cc8ca24b2f0ff9181259eaf765e299cfae
Hidehiro Nagaoka <kuinige@gmail.com>
2019-08-30 20:51:54 +0900

    React todo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,60 @@
 import React from 'react';
 class App extends React.Component {
+  state = {
+    todo: '',
+    todos: [{
+      text: 'A new todo name',
+      done: false,
+    }],
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      todo: event.currentTarget.value,
+    });
+  }
+
+  handleCheckbox = (index) => {
+    const { todos } = this.state;
+    todos[index].done = !todos[index].done;
+    this.setState({
+      todos,
+    });
+  }
+
+  handleSubmit = (event) => {
+    // grab original todos from state
+    const { todos } = this.state;
+    // todo text is result
+    // append new todo with default state to todos
+    this.setState({
+      todo: '',
+      todos: [
+        {
+          text: event.currentTarget.todo.value,
+          done: false,
+        },
+        ...todos,
+      ],
+    });
+    event.preventDefault();
+  }
+
+  handleRemove = (index) => {
+    // grab original todos from state
+    const { todos } = this.state;
+    // create an array excluding the array value based on the index
+    this.setState({
+      todo: '',
+      todos: [
+        ...todos.slice(0, index),
+        ...todos.slice(index + 1),
+      ],
+    });
+  }
+
   render() {
+    const { todo, todos } = this.state;
     return (
       <div className="App">
         <div className="container-fluid">
@@ -9,37 +63,46 @@ class App extends React.Component {
               <h1>React Todo App</h1>
             </header>
             <main className="col col-12">
-              <form style={{ marginBottom: '20px' }}>
-                <input className="form-control"  type="text" placeholder="Enter todo here... [Press Enter]" autoComplete="off" />
+              <form onSubmit={this.handleSubmit} style={{ marginBottom: '20px' }}>
+                <input name="todo" onChange={this.handleChange} value={this.state.todo}className="form-control" type="text" placeholder="Enter todo here... [Press Enter]" autoComplete="off" />
               </form>
               <ul className="todos list-groups" style={{ padding: 0 }}>
-                <li className="todo list-group-item">
-                  <input className="form-control" type="checkbox" />
-                  <span style={{
-                    top: 0,
-                    bottom: 0,
-                    left: '3rem',
-                    right: '5rem',
-                    lineHeight: '62px',
-                    display: 'block',
-                    position: 'absolute'
-                  }}
-                  >
-                    Write this code
-                  </span>
-                  <button type="button"
-                    className="btn btn-sm btn-danger"
-                    style={{
-                      position: 'absolute',
-                      top: 0,
-                      bottom: 0,
-                      right: '1.25rem',
-                      margin: 'auto 0',
-                      height: '25px',
-                      paddingTop: 0,
-                      paddingBottom: 0,
-                    }}>&times;</button>
-                </li>
+                {(todos.length === 0)
+                  ? (<li className="todo list-group-item">No todos yet</li>)
+                  : (todos.map((item, key) => (
+                    <li checked={item.done} key={`list-${(key + 1)}`} className="todo list-group-item">
+                      <input onChange={() => this.handleCheckbox(key)} checked={item.done} className="form-control" type="checkbox" />
+                      <span style={{
+                        top: 0,
+                        bottom: 0,
+                        left: '3rem',
+                        right: '5rem',
+                        lineHeight: '62px',
+                        display: 'block',
+                        position: 'absolute',
+                        textDecoration: (item.done) ? 'line-through' : 'none',
+                      }}
+                      >
+                        {item.text}
+                      </span>
+                      <button
+                        onClick={() => this.handleRemove(key)}
+                        type="button"
+                        className="btn btn-sm btn-danger"
+                        style={{
+                          position: 'absolute',
+                          top: 0,
+                          bottom: 0,
+                          right: '1.25rem',
+                          margin: 'auto 0',
+                          height: '25px',
+                          paddingTop: 0,
+                          paddingBottom: 0,
+                        }}>&times;
+                      </button>
+                    </li>
+                  )))
+                }
               </ul>
             </main>
           </div>
