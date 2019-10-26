5443f8ee192c2f5c093029d6faf656269c113006
牧紗理菜 <sarina@Sarina.local>
2019-10-26 16:17:48 +0900

    設定

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDQkm8mkAYb2bAEEN0al-1ImI0hzEbIKhQ",
-    authDomain: "react-nagaoka-20191013.firebaseapp.com",
-    databaseURL: "https://react-nagaoka-20191013.firebaseio.com",
-    projectId: "react-nagaoka-20191013",
-    storageBucket: "react-nagaoka-20191013.appspot.com",
-    messagingSenderId: "879652289929",
-    appId: "1:879652289929:web:e4e9bc3bd65163bc4bb558",
-    measurementId: "G-J2B4BSSHTD"
+    apiKey: "AIzaSyCIoIfKvIQ16fQ9n9ycvXb3P5mo2CFGk60",
+    authDomain: "sarina-todo.firebaseapp.com",
+    databaseURL: "https://sarina-todo.firebaseio.com",
+    projectId: "sarina-todo",
+    storageBucket: "sarina-todo.appspot.com",
+    messagingSenderId: "247938679940",
+    appId: "1:247938679940:web:405aaa0e92fca58109eed1",
+    measurementId: "G-QYD6QRKBS9"
 };
 const firebaseApp = firebase.initializeApp(config);
 export const firestore = firebaseApp.firestore();
